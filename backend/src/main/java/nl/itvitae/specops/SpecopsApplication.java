f931e64c4eb16d1e94a13f1779ba86118c600c7b
Thomas Vrielink <thomasvrielink92@gmail.com>
2024-02-19 14:32:57 +0100

    basic spring project with securityconfig and jwt dependencies added

---
+++ backend/src/main/java/nl/itvitae/specops/SpecopsApplication.java
@@ -0,0 +1,13 @@
+package nl.itvitae.specops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecopsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecopsApplication.class, args);
+	}
+
+}
