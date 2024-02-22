d5b0bd52c22cf10cbf9753f9b77175bc0707aa73
Thomas Vrielink <thomasvrielink92@gmail.com>
2024-02-22 15:37:51 +0100

    added routing to /taskmanagement for the task manager

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { TaskProvider } from './components/common/TaskContext.tsx'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SupervisorTaskManagement from './components/supervisor-task-management/SupervisorTaskManagement.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <TaskProvider>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/taskmanager" element={<SupervisorTaskManagement />} />
+    </Routes>
+    </BrowserRouter>
   </TaskProvider>,
 )
