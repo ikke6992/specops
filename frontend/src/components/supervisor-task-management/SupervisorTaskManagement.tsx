acc8520623a2c033a8dc538334df67b0beff91ea
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-02-22 14:21:58 +0100

    Simple Header

--- frontend/src/components/supervisor-task-management/SupervisorTaskManagement.tsx
+++ frontend/src/components/supervisor-task-management/SupervisorTaskManagement.tsx
@@ -4,7 +4,14 @@ const SupervisorTaskManagement = () => {
   return (
     <main className="h-screen">
       {/* Search & Filter Section */}
-      <section className="bg-slate-500" style={{ height: "20%" }}></section>
+      <section
+        className="bg-slate-500 flex flex-row justify-center items-center"
+        style={{ height: "20%" }}
+      >
+        <h1 className="text-3xl text-slate-950 font-black uppercase">
+          Task Manager
+        </h1>
+      </section>
       {/* Task List Section */}
       <section
         className="p-8 bg-gray-400 grid grid-cols-4 grid-rows-2 gap-5"
@@ -25,11 +32,11 @@ const SupervisorTaskManagement = () => {
         style={{ height: "10%" }}
       >
         <div></div>
-        <div className="p-2 flex flex-row justify-center content-center">
-          <button className="pr-4 pl-4 bg-cyan-600 hover:bg-cyan-300 border-gray-300 border-2 text-white">
+        <div className="p-2 flex flex-row justify-center items-center">
+          <button className="p-3 bg-cyan-600 hover:bg-cyan-300 border-gray-300 border-2 text-white">
             ←
           </button>
-          <button className="ml-4 pr-4 pl-4 bg-cyan-600 hover:bg-cyan-300 border-gray-300 border-2 text-white">
+          <button className="ml-4 p-3 bg-cyan-600 hover:bg-cyan-300 border-gray-300 border-2 text-white">
             →
           </button>
         </div>
