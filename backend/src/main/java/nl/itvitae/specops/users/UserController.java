7bfb85bf4c891d1cf0759e1201a44790374792d6
Thomas Vrielink <thomasvrielink92@gmail.com>
2024-02-22 10:55:39 +0100

    Fixed axios request to database
    -Added @CrossOrigin in UserController to prevent CORS from rejecting request
    - Put all relevant code within the App so useState can be found
    - Amended the link in getAllUsers to send the request to localhost:8080

--- backend/src/main/java/nl/itvitae/specops/users/UserController.java
+++ backend/src/main/java/nl/itvitae/specops/users/UserController.java
@@ -1,11 +1,13 @@
 package nl.itvitae.specops.users;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
