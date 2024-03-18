4487efe180a16a5d0bc60d4f6926a82e8fb66412
Thomas Vrielink <thomasvrielink92@gmail.com>
2024-03-18 15:29:39 +0100

    Tooltips activation fix
    Tooltips now only activate when hovering the i-icon, no longer for the entire timeframe/interval field

--- frontend/src/components/manage-tasks/fields/IntervalField.tsx
+++ frontend/src/components/manage-tasks/fields/IntervalField.tsx
@@ -1,14 +1,13 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { TaskModalContext } from "../../../contexts/TaskModalContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 const IntervalField = () => {
   const { interval, setInterval } = useContext(TaskModalContext);
-  const [focus, setFocus] = useState(false);
 
   return (
-    <div className="group">
+    <div style={{ display: "flex" }}>
       <label>
         Interval:{" "}
         <input
@@ -16,12 +15,14 @@ const IntervalField = () => {
           value={interval}
           onChange={(e) => setInterval(parseInt(e.target.value))}
         />
+      </label>
+      <div className="group">
         <FontAwesomeIcon className="pr-1" icon={faInfoCircle} />
         <span className="absolute scale-0 bg-black text-xs text-white rounded-lg p-2 mt-2 group-hover:scale-100">
           The amount of days between the last execution date and the next
           deadline
         </span>
-      </label>
+      </div>
     </div>
   );
 };
--- frontend/src/components/manage-tasks/fields/TimeframeField.tsx
+++ frontend/src/components/manage-tasks/fields/TimeframeField.tsx
@@ -1,14 +1,13 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { TaskModalContext } from "../../../contexts/TaskModalContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 const TimeframeField = () => {
   const { timeframe, setTimeframe } = useContext(TaskModalContext);
-  const [focus, setFocus] = useState(false);
 
   return (
-    <div className="group">
+    <div style={{ display: "flex" }}>
       <label>
         Timeframe:{" "}
         <input
@@ -16,17 +15,13 @@ const TimeframeField = () => {
           value={timeframe}
           onChange={(e) => setTimeframe(parseInt(e.target.value))}
         />
-        <FontAwesomeIcon
-          className="pr-1"
-          icon={faInfoCircle}
-          onClick={() => setFocus(!focus)}
-        />
-        <span
-          className="absolute scale-0 bg-black text-xs text-white rounded-lg p-2 mt-2 group-hover:scale-100"
-        >
+      </label>
+      <div className="group">
+        <FontAwesomeIcon className="pr-1 group" icon={faInfoCircle} />
+        <span className="absolute scale-0 bg-black text-xs text-white rounded-lg p-2 mt-2 group-hover:scale-100">
           The amount of days before the next deadline the task becomes active
         </span>
-      </label>
+      </div>
     </div>
   );
 };
