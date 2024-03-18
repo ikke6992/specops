65b05386ae2f84679aa278b888095dd8d9023a13
Thomas Vrielink <thomasvrielink92@gmail.com>
2024-03-18 14:16:32 +0100

    Add infobubbles to create/edit task
    Infobubbles have been added to create/edit task to explain what timeframe and interval do

--- frontend/src/components/manage-tasks/fields/IntervalField.tsx
+++ frontend/src/components/manage-tasks/fields/IntervalField.tsx
@@ -1,8 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { TaskModalContext } from "../../../contexts/TaskModalContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 const IntervalField = () => {
   const { interval, setInterval } = useContext(TaskModalContext);
+  const [focus, setFocus] = useState(false);
 
   return (
     <label>
@@ -12,6 +15,18 @@ const IntervalField = () => {
         value={interval}
         onChange={(e) => setInterval(parseInt(e.target.value))}
       />
+      <FontAwesomeIcon
+        className="pr-1"
+        icon={faInfoCircle}
+        onClick={() => setFocus(!focus)}
+      />
+      <p
+        className={
+          focus ? "bg-black text-xs text-white rounded-lg p-2 mt-2" : "hidden"
+        }
+      >
+        The amount of days between the last execution date and the next deadline
+      </p>
     </label>
   );
 };
--- frontend/src/components/manage-tasks/fields/TimeframeField.tsx
+++ frontend/src/components/manage-tasks/fields/TimeframeField.tsx
@@ -1,8 +1,11 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { TaskModalContext } from "../../../contexts/TaskModalContext";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 
 const TimeframeField = () => {
   const { timeframe, setTimeframe } = useContext(TaskModalContext);
+  const [focus, setFocus] = useState(false);
 
   return (
     <label>
@@ -12,6 +15,18 @@ const TimeframeField = () => {
         value={timeframe}
         onChange={(e) => setTimeframe(parseInt(e.target.value))}
       />
+      <FontAwesomeIcon
+        className="pr-1"
+        icon={faInfoCircle}
+        onClick={() => setFocus(!focus)}
+      />
+      <p
+        className={
+          focus ? "bg-black text-xs text-white rounded-lg p-2 mt-2" : "hidden"
+        }
+      >
+        The amount of days before the next deadline the task becomes active
+      </p>
     </label>
   );
 };
