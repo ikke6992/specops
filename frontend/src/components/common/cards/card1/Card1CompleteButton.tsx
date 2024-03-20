1c2ee0b28309d545e05662c5ffa89a9fea27df07
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-03-20 12:19:14 +0100

    Bigger checkoff + replacing info button

--- frontend/src/components/common/cards/card1/Card1CompleteButton.tsx
+++ frontend/src/components/common/cards/card1/Card1CompleteButton.tsx
@@ -12,25 +12,30 @@ const Card1CompleteButton = ({ completeTask, completeStatus }: Props) => {
   const [askConfirm, setAskConfirm] = useState(false);
   return (
     <>
-    <div className="flex p-1 gap-x-1 items-center relative">
-      <span className="font-semibold">Completed:</span>
-      <input
-        type="checkbox"
-        checked={!completeStatus}
-        value=""
-        className={`appearance-none shrink-0 w-4 h-4 border-2 rounded-sm bg-white checked:bg-green-100 border-black/50 hover:border-green-500 checked:border-green-500 no-click ${
-          !completeStatus && "pointer-events-none"
-        }`}
-        onClick={(e) => setAskConfirm(true)}
-      />
-      {!completeStatus && (
-        <FontAwesomeIcon
-          className="absolute right-1.5 w-3 h-3 text-green-500 pointer-events-none"
-          icon={faCheck}
+      <div className="flex p-1 gap-x-1 items-center relative">
+        <span className="font-semibold">Completed:</span>
+        <input
+          type="checkbox"
+          checked={!completeStatus}
+          value=""
+          className={`appearance-none shrink-0 w-6 h-6 border-2 rounded-sm bg-white checked:bg-green-100 border-black/50 hover:border-green-500 checked:border-green-500 no-click ${
+            !completeStatus && "pointer-events-none"
+          }`}
+          onClick={() => setAskConfirm(true)}
+        />
+        {!completeStatus && (
+          <FontAwesomeIcon
+            className="absolute right-2 w-3.5 h-3.5 text-green-500 pointer-events-none"
+            icon={faCheck}
+          />
+        )}
+      </div>
+      {askConfirm && (
+        <TaskComplete
+          completeTask={completeTask}
+          close={() => setAskConfirm(false)}
         />
       )}
-    </div>
-    {askConfirm && (<TaskComplete completeTask={completeTask} close={() => setAskConfirm(false)} />)}
     </>
   );
 };
--- frontend/src/components/manage-tasks/fields/IntervalField.tsx
+++ frontend/src/components/manage-tasks/fields/IntervalField.tsx
@@ -8,19 +8,21 @@ const IntervalField = () => {
 
   return (
     <>
-      <label>Interval: </label>
-      <input
-        type="number"
-        className="border rounded-md p-1 outline-none border-gray-400"
-        value={interval}
-        onChange={(e) => setInterval(parseInt(e.target.value))}
-      />
-      <div className="group">
-        <FontAwesomeIcon className="pr-1" icon={faInfoCircle} />
-        <span className="absolute scale-0 bg-black text-xs text-white rounded-lg p-2 mt-2 group-hover:scale-100">
-          The amount of days between the last execution date and the next
-          deadline
-        </span>
+      <label>Interval (days): </label>
+      <div className="flex flex-row items-center">
+        <input
+          type="number"
+          className="border rounded-md p-1 outline-none border-gray-400"
+          value={interval}
+          onChange={(e) => setInterval(parseInt(e.target.value))}
+        />
+        <div className="group">
+          <FontAwesomeIcon className="pr-1 ml-2" icon={faInfoCircle} />
+          <span className="absolute scale-0 bg-black text-xs text-white rounded-lg p-2 mt-2 group-hover:scale-100 min-w-60">
+            The number of days between the last execution date and the next
+            deadline
+          </span>
+        </div>
       </div>
     </>
   );
