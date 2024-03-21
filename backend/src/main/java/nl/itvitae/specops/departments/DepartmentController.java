628bd1c390e2a8f3e1e44a7784ac42343308c732
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-03-21 11:06:08 +0100

    Editing departments

--- backend/src/main/java/nl/itvitae/specops/departments/DepartmentController.java
+++ backend/src/main/java/nl/itvitae/specops/departments/DepartmentController.java
@@ -1,9 +1,13 @@
 package nl.itvitae.specops.departments;
 
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +20,7 @@ import java.util.List;
 @CrossOrigin
 public class DepartmentController {
   private final DepartmentService departmentService;
+  private final DepartmentRepository departmentRepository;
 
   @GetMapping
   public List<Department> getAll() {
@@ -24,8 +29,17 @@ public class DepartmentController {
 
   private record DepartmentCreateData(String name) {}
 
-  @PostMapping
-  public Department create(@RequestBody DepartmentCreateData req) {
-    return departmentService.save(req.name());
+  @PostMapping("/create")
+  public ResponseEntity<Department> create(@RequestBody DepartmentCreateData req) {
+    return ResponseEntity.ok(departmentService.save(req.name()));
+  }
+
+  @PutMapping("/edit/{id}")
+  public ResponseEntity<Department> edit(
+      @RequestBody DepartmentCreateData req, @PathVariable UUID id) {
+    if (departmentRepository.findById(id).isEmpty()) return ResponseEntity.notFound().build();
+    final Department department = departmentRepository.findById(id).get();
+    department.setName(req.name());
+    return ResponseEntity.ok(departmentRepository.save(department));
   }
 }
