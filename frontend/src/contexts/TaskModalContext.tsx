bb1670ada828bff1c759fe51e7a4361b00a59dfb
Tuyan T <tuyantat@gmail.com>
2024-03-22 06:47:59 +0100

    Update

--- frontend/src/contexts/TaskModalContext.tsx
+++ frontend/src/contexts/TaskModalContext.tsx
@@ -11,11 +11,11 @@ type ContextType = {
   setInterval: (interval: number) => void;
   setDeadline: (deadline: string) => void;
   setDept: (dept: string) => void;
-  // validName: boolean;
-  // validTimeFrame: boolean;
-  // validInterval: boolean;
-  // validDeadline: boolean;
-  // valid: boolean;
+  validName: boolean;
+  validTimeFrame: boolean;
+  validInterval: boolean;
+  validDeadline: boolean;
+  valid: boolean;
 };
 type ProviderType = FC<{ children: ReactNode }>;
 
@@ -40,11 +40,11 @@ export const TaskModalContext = createContext<ContextType>({
   setDeadline: (deadline) => {
     return deadline;
   },
-  // validName: false,
-  // validTimeFrame: false,
-  // validInterval: false,
-  // validDeadline: false,
-  // valid: false,
+  validName: false,
+  validTimeFrame: false,
+  validInterval: false,
+  validDeadline: false,
+  valid: false,
 });
 
 export const TaskModalProvider: ProviderType = ({ children }) => {
@@ -53,19 +53,23 @@ export const TaskModalProvider: ProviderType = ({ children }) => {
   const [timeframe, setTimeframe] = useState<number>(0);
   const [interval, setInterval] = useState<number>(0);
   const [deadline, setDeadline] = useState<string>("");
-  // const [validName, setValidName] = useState(false);
-  // const [validTimeFrame, setValidTimeFrame] = useState(false);
-  // const [validInterval, setValidInterval] = useState(false);
-  // const [validDeadline, setValidDeadline] = useState(false);
-  // const [valid, setValid] = useState(false);
+  const [validName, setValidName] = useState(false);
+  const [validTimeFrame, setValidTimeFrame] = useState(false);
+  const [validInterval, setValidInterval] = useState(false);
+  const [validDeadline, setValidDeadline] = useState(false);
+  const [valid, setValid] = useState(false);
 
-  // useEffect(() => {
-  //   setValidName(taskName.length > 0);
-  //   setValidTimeFrame(timeframe > 1);
-  //   setValidInterval(interval > 0);
-  //   setValidDeadline(Date.parse(deadline) >= Date.now());
-  //   setValid(validName && validTimeFrame && validInterval && validDeadline);
-  // }, [taskName, timeframe, interval, deadline]);
+  useEffect(() => {
+    const updateValidations = async () => {
+    setValidName(taskName.length > 0);
+    setValidTimeFrame(timeframe > 1);
+    setValidInterval(interval > 0);
+    setValidDeadline(Date.parse(deadline) >= Date.now());
+    setValid(validName && validTimeFrame && validInterval && validDeadline);
+    };
+
+    updateValidations();
+  }, [taskName, timeframe, interval, deadline]);
 
   return (
     <TaskModalContext.Provider
@@ -80,11 +84,11 @@ export const TaskModalProvider: ProviderType = ({ children }) => {
         setTimeframe,
         setInterval,
         setDeadline,
-        // validName,
-        // validTimeFrame,
-        // validInterval,
-        // validDeadline,
-        // valid,
+        validName,
+        validTimeFrame,
+        validInterval,
+        validDeadline,
+        valid,
       }}
     >
       {children}
