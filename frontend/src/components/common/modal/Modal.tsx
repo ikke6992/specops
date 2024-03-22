7d067f680a9c39c8e51ae8f7bcf2549fd322ef55
BaloeTheBear <76252234+BaloeTheBear@users.noreply.github.com>
2024-03-22 09:59:53 +0100

    no-click

--- frontend/src/components/common/modal/Modal.tsx
+++ frontend/src/components/common/modal/Modal.tsx
@@ -17,27 +17,27 @@ export default function Modal(props: {
 
   return (
     <>
-      <div className="justify-center items-center flex fixed inset-0 z-50">
-        <div className="w-fit">
+      <div className="justify-center items-center flex fixed inset-0 z-50 no-click">
+        <div className="w-fit no-click">
           {/*content*/}
-          <div className="border-0 rounded-lg border-2 shadow-xl relative bg-white">
+          <div className="border-0 rounded-lg border-2 shadow-xl relative bg-white no-click">
             {/*header*/}
-            <div className="flex justify-between border-b">
+            <div className="flex justify-between border-b no-click">
               <ModalHeader name={props.name} />
               <ModalClose close={props.close} />
             </div>
             {/*body*/}
-            <div className="p-6 w-fit">{props.form}</div>
+            <div className="p-6 w-fit no-click">{props.form}</div>
             {/*footer*/}
             {props.name !== "Share link" && (
-              <div className={`flex justify-start py-4 border-t`}>
+              <div className={`flex justify-start py-4 border-t no-click`}>
                 <ModalSubmit name={props.name} handleSubmit={handleSubmit} />
               </div>
             )}
           </div>
         </div>
       </div>
-      <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+      <div className="opacity-25 fixed inset-0 z-40 bg-black no-click"></div>
     </>
   );
 }
--- frontend/src/components/common/modal/ModalSubmit.tsx
+++ frontend/src/components/common/modal/ModalSubmit.tsx
@@ -4,7 +4,7 @@ const ModalSubmit = (props: {
 }) => {
   return (
     <button
-      className="h-11 w-26 px-4 py-2 text-sm font-medium text-white bg-green-500 hover:bg-green-600 border-2 border-green-700 ml-4 rounded-md"
+      className="h-11 w-26 px-4 py-2 text-sm font-medium text-white bg-green-500 hover:bg-green-600 border-2 border-green-700 ml-4 rounded-md no-click"
       type="button"
       onClick={(e) => props.handleSubmit(e)}
     >
